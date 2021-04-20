fb84b11df0a33d3c953f61bc436a65b3da69ab16
Speak91 <i-valentin@mail.ru>
2021-04-20 13:35:53 +0900

    Выполнено ДЗ

---
+++ HomeWork1/HomeWork/HomeWork_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+namespace HomeWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Привет {name}!, ждем 5 секунд" );
+            Thread.Sleep(6000);
+            Console.WriteLine($"До свидания {name}, для завершения нажми любую кнопку");
+            Console.ReadKey();
+        }
+    }
+}
