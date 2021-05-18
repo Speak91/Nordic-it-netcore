244bb4a87bad2dae3a78502015ce9a91185e041e
Speak91 <i-valentin@mail.ru>
2021-05-18 22:48:09 +0900

    Загружена домашняя работа №1 6 урок

---
+++ Lesson_6/HomeWork/HomeWork/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число не превышающее 2 миллиардов");
+            int input = 0;
+            int count = 0;
+            while (true)
+            {  
+               
+                if (int.TryParse(Console.ReadLine(), out input) == false)
+                {
+                    Console.WriteLine("Ошибка System.FormatException! Попробуйте ещё раз:");
+                    continue;
+                }
+                if (input > 2_000_000_000)
+                {
+                    Console.WriteLine("Ошибка System.OverflowException! Попробуйте ещё раз:");
+                    continue;
+                }
+                else
+                {
+                    while (input != 0)
+                    {
+                        if ((input % 10) % 2 == 0)
+                        {
+                            count++;
+                        }
+                        input = input / 10;
+                    }
+                   
+                }
+                Console.WriteLine("Количество четных цифр: {0}", count);
+                Console.ReadKey();
+                break;
+            }
+            
+          
+        }
+    }
+}
