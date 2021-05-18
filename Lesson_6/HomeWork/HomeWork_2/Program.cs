5f4352c2cd458108f114e2692c6f542ad2c8243c
Speak91 <i-valentin@mail.ru>
2021-05-19 01:25:20 +0900

    Загружена домашняя работа №2 6 урок

---
+++ Lesson_6/HomeWork/HomeWork_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal downPaymentAmount = 0;
+            decimal percentageDailyIncome = 0;
+            decimal accumulationAmount = 0;
+            decimal result = 0;
+            int numberOfDays = 0;
+            try
+            {
+                Console.WriteLine("Введите сумму первоначального взноса в формате 100,25 где 100 рубли а 25 копейки");
+                downPaymentAmount = Test();
+                Console.WriteLine(percentageDailyIncome * 100);
+                Console.WriteLine("Введите ежедневный процент дохода в виде десятичной дроби (1% = 0.01):");
+                percentageDailyIncome = Test();
+                Console.WriteLine("Введите желаемую сумму накопления в рублях:");
+                accumulationAmount = Test();
+                result = downPaymentAmount * percentageDailyIncome;
+
+                while (downPaymentAmount < accumulationAmount)
+                {
+                    numberOfDays++;
+                    downPaymentAmount = downPaymentAmount + result;
+                }
+                Console.WriteLine($"Необходимое количество дней для накопления {accumulationAmount}Р при ставке {Math.Round(result, 1)}% составит {numberOfDays}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Вы ввели не верное значение!");
+            }
+            catch (System.OverflowException)
+            {
+                Console.WriteLine($"Введенное число превышает допустимое значение");
+            }
+          
+          
+
+        }
+        static decimal Test()
+        {
+            decimal testValue = 0;
+            while (decimal.TryParse(Console.ReadLine(), out testValue) == false || testValue <0)
+            {
+                Console.WriteLine("Введено некоректное значение повторите попытку");
+            }
+            return testValue;
+        }
+    }
+}
