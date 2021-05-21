9b1c193e3cb01950d690f3e2a08cbaf81ac89b02
Speak91 <i-valentin@mail.ru>
2021-05-21 21:49:03 +0900

    Загружена домашнаяя работа №2 7 урока

---
+++ Lesson_7/HomeWork/HomeWork_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите непустую строку");
+
+            string output = CheckingForBlankCharacterBeginningLine();
+
+            output = output.ToLower();
+
+            char[] sReverse = output.ToCharArray();
+            Array.Reverse(sReverse);
+            output = new string(sReverse);
+
+            Console.WriteLine($"В перевернутом виде выглядит вот так: " + output);
+        }
+
+        static string CheckingForBlankCharacterBeginningLine()
+        {
+            string output = string.Empty;
+            while ((output = Console.ReadLine()).StartsWith(" "))
+            {
+                Console.WriteLine("Пустой ввод или в начале строки стоит пробел, повторите попытку");
+            }
+            return output;
+        }
+    }
+}
