d804792903d8b44ccb0864a009a23e192ca369f8
Speak91 <i-valentin@mail.ru>
2021-05-21 11:49:47 +0900

    Исправленна домашняя работа 5 урока

--- Lesson_5/HomeWork/HomeWork/Program.cs
+++ Lesson_5/HomeWork/HomeWork/Program.cs
@@ -12,87 +12,70 @@ namespace HomeWork
         }
         static void Main(string[] args)
         {
+            const double pi = 3.14;
+
             double rectangleLength = 0;
             double rectangleHeight = 0;
-            double rectangleArea = 0;
-            double rectanglePerimeter = 0;
-            
             double circleDiameter = 0;
-            double circleArea = 0;
-            double circlePerimeter = 0;
-
             double triangleSide = 0;
-            double triangleArea = 0;
-            double trianglePerimeter = 0;
+            double area = 0;
+            double perimetr = 0;
 
-            string triangle = "Треугольника";
-            string circle = "Круга";
-            string rectangle = "Прямоугольника";
-            string square = "Площадь";
-            const double pi = 3.14; 
-            
+
+
             Console.WriteLine("Выберите тип фигуры: 1 - круг, 2 - равносторонний треугольник, 3 - прямоугольник");
-            
+
             try
             {
                 int shapesNumber = Convert.ToInt32(Console.ReadLine());
                 Shapes shapes = (Shapes)shapesNumber;
-                
+
                 switch (shapes)
                 {
                     case Shapes.Сircle:
                         Console.WriteLine("Выбран круг");
                         Console.WriteLine("Введите диаметр");
                         circleDiameter = Convert.ToDouble(Console.ReadLine());
-                        circleArea = Math.Pow(circleDiameter, 2) / 4 * pi;
-                        circlePerimeter = circleDiameter * pi;
-                        Print(circleArea, circlePerimeter);
+                        area = Math.Pow(circleDiameter, 2) / 4 * pi;
+                        perimetr = circleDiameter * pi;
                         break;
                     case Shapes.Triangle:
                         Console.WriteLine("Выбран треугольник");
                         Console.WriteLine("Введите длину стороны");
-                        
+
                         triangleSide = Convert.ToDouble(Console.ReadLine());
-                        triangleArea = Math.Pow(triangleSide, 2) * Math.Sqrt(3) / 4;
-                        trianglePerimeter = 3 * triangleSide;
-                        Print(triangleArea, trianglePerimeter);
+                        area = Math.Pow(triangleSide, 2) * Math.Sqrt(3) / 4;
+                        perimetr = 3 * triangleSide;
                         break;
-                    
+
                     case Shapes.Rectangle:
                         Console.WriteLine("Выбран прямоугольник");
                         Console.WriteLine("Введите длину прямоугольника:");
-                       
                         rectangleLength = Convert.ToDouble(Console.ReadLine());
                         Console.WriteLine("Введите высоту прямоугольника");
                         rectangleHeight = Convert.ToDouble(Console.ReadLine());
-                        
-                        rectangleArea = rectangleLength * rectangleHeight;
-                        rectanglePerimeter = 2 * (rectangleLength + rectangleHeight);
-                        Print(rectangleArea, rectanglePerimeter);
-                        
+                        area = rectangleLength * rectangleHeight;
+                        perimetr = 2 * (rectangleLength + rectangleHeight);
                         break;
                     default:
                         Console.WriteLine("Такой фигуры нету, программа завершает свою работу");
                         break;
                 }
+                Console.WriteLine($"Площадь выбранной фигуры = {area}");
+                Console.WriteLine($"Периметр выбранной фигуры = {perimetr}");
             }
-            
+
             catch (FormatException)
             {
                 Console.WriteLine("Введено некорректное значение, программа завершает свою работу");
             }
-            
+
             catch (Exception e)
             {
                 Console.WriteLine("Что то пошло не так, покажите разработчику строчку ниже");
                 Console.WriteLine(e);
             }
 
-        }
-        static void Print(double square, double perimetr)
-        {
-            Console.WriteLine($"Площадь выбранной фигуры составляет = {square}" );
-            Console.WriteLine($"Периметр выбранной фигуры составляет = {perimetr}");
         }
     }
 }
