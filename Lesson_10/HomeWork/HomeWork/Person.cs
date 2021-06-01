7d0cd5dd1b14a1fd9f7ac232a04fe5414b55fff2
Speak91 <i-valentin@mail.ru>
2021-06-01 21:06:41 +0900

    Выполнено домашнее задание к 10 уроку

---
+++ Lesson_10/HomeWork/HomeWork/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Person
+    {
+        public string Name { get; set; }
+        private int _age;
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value >= 0 && value < 150)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new Exception("Возраст не может быть меньше 0 или больше 200");
+                }
+            }
+        }
+
+        public string AgeАfterFourYears
+        {
+            get
+            {
+                return $"{_age + 4}";
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return $"{Name} ваш возраст через 4 года будет = {AgeАfterFourYears}";
+            }
+        }
+
+        public void Input(int personNumber)
+        {
+            Console.Write($"Введите имя №{personNumber + 1}: ");
+            Name = Console.ReadLine();
+            Console.Write($"Введите возраст №{personNumber + 1}: ");
+            Age = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Lesson_10/HomeWork/HomeWork/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int amountOfPeople = 3;
+            Person[] persons = new Person[amountOfPeople];
+            for (int i = 0; i < amountOfPeople; i++)
+            {
+                persons[i] = new Person();
+                try
+                {
+                    persons[i].Input(i);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Произощла ошибка {e.Message} повторите попытку");
+                    i--;
+                }
+            }
+            for (int i = 0; i < amountOfPeople; i++)
+            {
+                Console.WriteLine(persons[i].Description);
+            }
+        }
+    }
+}
